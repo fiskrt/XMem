e1ecf6b6cc92b31c763052f41b860c2a00c62428
Filip bayes <filipsk@bayes.ita.chalmers.se>
2023-03-09 12:47:29 +0100

    Can now compare two videos side-by-side

--- create_video.py
+++ create_video.py
@@ -5,27 +5,46 @@ import os
 from PIL import Image
 
 
+counter = 0
+compare = True
+dim=0
 
-category = 'bmx-trees'
-result = 'd17_onlydice'
+for i, category in enumerate(['pigs']):
+    result = 'd17_both_scratch_NOCROP_warmup_s3_50k'
+    result2 = 'd17_bcedice_scratch_NOCROP_s3_50k'
 
-path_mask = f'./results/{result}/{category}/'
-path_img = f'../DAVIS/2017/trainval/JPEGImages/480p/{category}/'
+    path_mask = f'./results/{result}/{category}/'
+    path_mask2 = f'./results/{result2}/{category}/'
+    path_img = f'../DAVIS/2017/trainval/JPEGImages/480p/{category}/'
 
-fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-#fourcc = cv2.VideoWriter_fourcc(*'avc1')
-with Image.open(path_mask+'00000.png') as m:
-    writer = cv2.VideoWriter(f"videos/{result}-{category}.avi",fourcc, 6, m.size)
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    #fourcc = cv2.VideoWriter_fourcc(*'avc1')
+    if i == 0:
+        with Image.open(path_mask+'00000.png') as m:
+            size = m.size
+            if compare:
+                size = (2*size[0], size[1]) if dim==1 else (size[0], 2*size[1])
+            # size is passed in the reverse way (WxH)
+            writer = cv2.VideoWriter(f"videos/{result}-{category}-COMP0.avi",fourcc, 6, size)
 
 
-for mask in os.listdir(path_mask):
-    print(mask)
-    fname = mask.split('.')
-    with Image.open(os.path.join(path_mask, mask)) as m, Image.open(os.path.join(path_img, fname[0]+'.jpg')) as im:
-        frame = np.asarray(im)
-        m = np.asarray(m)
-        visualization = overlay_davis(frame, m) 
+    for mask in os.listdir(path_mask):
+        counter += 1
+        fname = mask.split('.')
+        with Image.open(os.path.join(path_mask, mask)) as m, Image.open(os.path.join(path_img, fname[0]+'.jpg')) as im:
+            frame = np.asarray(im)
+            m = np.asarray(m)
+            visualization = overlay_davis(frame, m) 
+
+        if compare:
+            with Image.open(os.path.join(path_mask2, mask)) as m2: 
+                m2 = np.asarray(m2)
+                visualization2 = overlay_davis(frame, m2) 
+                visualization = np.concatenate([visualization, visualization2], axis=dim)
+
         writer.write(visualization.astype('uint8'))
+
+print(f'Wrote video with {counter} frames')
 
 writer.release()
 
