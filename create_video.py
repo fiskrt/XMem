881960873b06d03eef98e282dabf9c1939afaa1f
Filip <filipskogh99@hotmail.com>
2023-04-04 13:10:33 +0200

    Always resize to (854, 480), show classes with top k biggest improvement
    - the eval dataset uses different sizes so resize all to the same

--- create_video.py
+++ create_video.py
@@ -1,20 +1,26 @@
 import cv2
 import numpy as np
-from inference.interact.interactive_utils import image_to_torch, index_numpy_to_one_hot_torch, torch_prob_to_numpy_mask, overlay_davis
+from inference.interact.interactive_utils import overlay_davis
 import os
 from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
+import pandas as pd
 
-def video(result, result2, compare=True, dim=0):
+def video(result, result2, mp4_out_name, compare=True, dim=0):
     """
         dim=0 means concat along y axis
         compare=True means to compare result and result2
     """
     counter = 0
-
-    for i, category in enumerate(['pigs', 'blackswan', 'india', 'breakdance']):
+    categories,scores = get_diff_classes(result, result2, topk=5)
+    print(categories)
+    size = (854, 480)
+#    categories = ['pigs', 'blackswan']
+    for i, category in enumerate(categories):
         # when compare==True result will be on top/left.
-
+        J, F, J2, F2 = scores[i]
         path_mask = f'./results/{result}/{category}/'
         path_mask2 = f'./results/{result2}/{category}/'
         path_img = f'../DAVIS/2017/trainval/JPEGImages/480p/{category}/'
@@ -23,39 +29,89 @@ def video(result, result2, compare=True, dim=0):
             fourcc = cv2.VideoWriter_fourcc(*'MJPG')
             #fourcc = cv2.VideoWriter_fourcc(*'avc1')
             with Image.open(path_mask+'00000.png') as m:
-                size = m.size
                 if compare:
-                    size = (2*size[0], size[1]) if dim==1 else (size[0], 2*size[1])
+                    full_size = (2*size[0], size[1]) if dim==1 else (size[0], 2*size[1])
                 # size is passed in the reverse way (WxH)
-                writer = cv2.VideoWriter(f"videos/{result}-{category}-multi.avi",fourcc, 6, size)
+                writer = cv2.VideoWriter(f"videos/{result}.avi",fourcc, 6, full_size)
 
-
-        for mask in os.listdir(path_mask):
-            counter += 1
+        font = ImageFont.truetype("FreeSans.ttf", 22)
+        for mask in sorted(os.listdir(path_mask)):
             fname = mask.split('.')
             with Image.open(os.path.join(path_mask, mask)) as m, Image.open(os.path.join(path_img, fname[0]+'.jpg')) as im:
+                if m.size != size:
+                    m = m.resize(size)
+                    im = im.resize(size)
+                im2 = im.copy()
+                draw = ImageDraw.Draw(im)
+                draw.text((0, 0), f'{result[-18:]} J:{J} F:{F}',(255,255,255),font=font)
                 frame = np.asarray(im)
                 m = np.asarray(m)
                 visualization = overlay_davis(frame, m) 
 
             if compare:
                 with Image.open(os.path.join(path_mask2, mask)) as m2: 
-                    m2 = np.asarray(m2)
-                    visualization2 = overlay_davis(frame, m2) 
-                    visualization = np.concatenate([visualization, visualization2], axis=dim)
+                    draw = ImageDraw.Draw(im2)
+                    draw.text((0, 0), f'{result2[-18:]} J:{J2} F:{F2}',(255,255,255),font=font)
+                    frame2 = np.asarray(im2)
 
+                    if m2.size != size:
+                        m2 = m2.resize(size)
+                    m2 = np.asarray(m2)
+                    visualization2 = overlay_davis(frame2, m2) 
+
+                    visualization = np.concatenate([visualization, visualization2], axis=dim)
+                    # convert from BGR to RGB
+                    visualization = visualization[..., ::-1].copy()
+
+            counter += 1
             writer.write(visualization.astype('uint8'))
 
     print(f'Wrote video with {counter} frames')
 
     writer.release()
+    os.system(f'/usr/bin/ffmpeg -i videos/{result}.avi -c:v h264 videos/{mp4_out_name}.mp4')
+    os.system(f'rm videos/{result}.avi')
 
 # The codec we want is not in cv2 pip version due to licensing
-#ffmpeg -i remote2.avi -c:v h264 out2.mp4
+# conda's ffmpeg uses libopenh264(does not work) instead of libx264
+# /usr/bin/ffmpeg -i in.avi -c:v h264 out.mp4
+
+def get_diff_classes(base, comp, topk=10):
+    """
+        Gets the categories in which the scores vary the most
+        between models base and comp.
+    """
+    base_path = f'results/{base}/per-sequence_results-val.csv'
+    comp_path = f'results/{comp}/per-sequence_results-val.csv'
+    base_df = pd.read_csv(base_path)
+    comp_df = pd.read_csv(comp_path)
+
+    base_df['J-Diff'] = base_df['J-Mean']-comp_df['J-Mean']
+    base_df['F-Diff'] = base_df['F-Mean']-comp_df['F-Mean']
+    base_df['Diff-Mean'] = abs(0.5*(base_df['J-Diff']+base_df['F-Diff']))
+    base_df['J-Mean2'] = comp_df['J-Mean']
+    base_df['F-Mean2'] = comp_df['F-Mean']
+
+    sorted_df = base_df.sort_values(by=['Diff-Mean'], ascending=False)
+    categories = sorted_df[['Sequence', 'J-Mean', 'F-Mean', 'J-Mean2', 'F-Mean2']]
+    cleaned_names = []
+    scores = []
+    for _, c, j, f, j2,f2 in categories.itertuples():
+        scores.append((j,f, j2, f2))
+        if c[-2] == '_':
+            if c[:-2] not in cleaned_names:
+                cleaned_names.append(c[:-2])
+        else:
+            if c not in cleaned_names:
+                cleaned_names.append(c)
+    return cleaned_names[:topk], scores[:topk]
 
 
 if __name__=='__main__':
     print('Running as script')
-    result = 'd17_both_scratch_NOCROP_warmup_s3_50k'
-    result2 = 'd17_bcedice_scratch_NOCROP_s3_50k'
-    video(result, result2)
+    mp4_out_name = 'Only_proj_vs_proj_and_pairwise_new3'
+    result = 'd17_Mar27_02.39.59_only_proj_batch2_679224_s3_110000'
+    result2 = 'd17_Mar23_01.37.01_no_tl_scale_loss_by_5_lr1e5_batch2_night_s3_110000'
+    video(result, result2, mp4_out_name)
+
+
